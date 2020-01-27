fa642e9253a4297de2d8868938d7f78ed6b51d1e
Lilian Gallon <swellchaser33@gmail.com>
2020-01-26 19:43:36 -0800

    created a cube class to simplify cube drawing

---
+++ js/Cube.js
@@ -0,0 +1,111 @@
+/**
+ * (c) 2020 Lilian Gallon, MIT License
+ * File creation: 01/23/2020
+ * UCSC, CSE160, Winter 2020
+ * https://nero.dev
+ *
+ * Required:
+ * - libs/
+ *
+ * Description:
+ *  This class is used to easily create a cube:
+ *    v6----- v5
+ *   /|      /|
+ *  v1------v0|
+ *  | |     | |
+ *  | |v7---|-|v4
+ *  |/      |/
+ *  v2------v3
+ */
+
+
+class Cube {
+    /**
+     * It initializes all the arrays needed.
+     * @param {WebGL2RenderingContext} gl WebGL Context
+     */
+    constructor(gl) {
+        this.gl = gl;
+
+        this.vertices = new Float32Array([
+            1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0,  // v0-v1-v2-v3 front
+            1.0, 1.0, 1.0,   1.0,-1.0, 1.0,   1.0,-1.0,-1.0,   1.0, 1.0,-1.0,  // v0-v3-v4-v5 right
+            1.0, 1.0, 1.0,   1.0, 1.0,-1.0,  -1.0, 1.0,-1.0,  -1.0, 1.0, 1.0,  // v0-v5-v6-v1 up
+           -1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0,  // v1-v6-v7-v2 left
+           -1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0,  // v7-v4-v3-v2 down
+            1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0   // v4-v7-v6-v5 back
+        ]);
+
+        this.colors = new Float32Array([
+            0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  0.4, 0.4, 1.0,  // v0-v1-v2-v3 front(blue)
+            0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  0.4, 1.0, 0.4,  // v0-v3-v4-v5 right(green)
+            1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  1.0, 0.4, 0.4,  // v0-v5-v6-v1 up(red)
+            1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  1.0, 1.0, 0.4,  // v1-v6-v7-v2 left
+            1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  1.0, 1.0, 1.0,  // v7-v4-v3-v2 down
+            0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0,  0.4, 1.0, 1.0   // v4-v7-v6-v5 back
+        ]);
+
+        this.indices = new Uint8Array([
+            0, 1, 2,    0, 2, 3,    // front
+            4, 5, 6,    4, 6, 7,    // right
+            8, 9,10,    8,10,11,    // up
+            12,13,14,  12,14,15,    // left
+            16,17,18,  16,18,19,    // down
+            20,21,22,  20,22,23     // back
+        ]);
+    }
+
+    /**
+     * It fills the buffer with the vertices to draw a cube.
+     */
+    build() {
+        let indexBuffer = this.gl.createBuffer();
+        if (!indexBuffer) {
+            console.error('Could not create a buffer');
+            return false;
+        }
+
+        this._bind(this.vertices, 3, this.gl.FLOAT, 'a_Position');
+        this._bind(this.colors  , 3, this.gl.FLOAT, 'a_Color');
+
+        this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, this.indices, this.gl.STATIC_DRAW);
+
+        return true;
+    }
+
+    /**
+     * It draws the cube.
+     * Make sure that you did not modify the WebGL buffer since the last
+     * build() call.
+     */
+    draw() {
+        this.gl.drawElements(this.gl.TRIANGLES, this.indices.length, this.gl.UNSIGNED_BYTE, 0);
+    }
+
+    /**
+     * It binds the given data to the specified variable in the GPU.
+     * @param {Float32Array} data the data,
+     * @param {GLint} num the size,
+     * @param {GLenum} type the variable type,
+     * @param {String} attr the variable name (attribute)
+     */
+    _bind(data, num, type, attr) {
+        let buffer = this.gl.createBuffer();
+        if (!buffer) {
+            console.error('Could not create a buffer');
+            return false;
+        }
+
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffer);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, data, this.gl.STATIC_DRAW);
+
+        let a_attr = this.gl.getAttribLocation(this.gl.program, attr);
+        if (a_attr < 0) {
+            console.error('Failed to get the storage location of ' + attr);
+            return 0;
+        }
+        this.gl.vertexAttribPointer(a_attr, num, type, false, 0, 0);
+        this.gl.enableVertexAttribArray(a_attr);
+    }
+}
