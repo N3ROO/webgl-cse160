d1a2a345529eda6d05d64c76a47133ebac45084c
Lilian Gallon <swellchaser33@gmail.com>
2020-02-14 19:07:32 -0800

    fixed clear issue

--- js/World.js
+++ js/World.js
@@ -170,7 +170,7 @@ class World {
      */
     clear() {
         this.gl.clearColor(0.0, 0.4, 1.0, 1.0);
-        this.gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
     }
 
     /**
